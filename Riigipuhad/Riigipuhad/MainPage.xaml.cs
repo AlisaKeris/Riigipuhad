920501edc00ab50186c59a6fd352b699ecaa21d8
Opilane TTHK <opilane@tthk.ee>
2020-10-09 09:58:41 +0300

    Add project files.

---
+++ Riigipuhad/Riigipuhad/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Riigipuhad
+{
+    public partial class MainPage : TabbedPage
+    {
+        Button btn;
+        public MainPage()
+        {
+            InitializeComponent();
+            btn = new Button();
+            btn.Clicked += Btn_Clicked1;
+        }
+
+        async void Btn_Clicked1(object sender, EventArgs e)
+        {
+            string answer = await DisplayActionSheet("Vali kuu", "Cancel", null, "Jaanuar", "Veebruar", "Marts", "April", "Mai", "Juuni", "Juuli", "August", "September", "Oktober", "November", "December");
+        }
+    }
+}
